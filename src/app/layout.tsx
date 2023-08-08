d4b28936d04723474648bb0b2569faa687dadf53
Monett <monet9812@gmail.com>
2023-08-08 15:59:56 +0900

    fix: change font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Noto_Sans_KR } from "next/font/google";
 import React from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Noto_Sans_KR({
+  weight: ["100", "300", "400", "500", "700", "900"],
+  subsets: ["latin"],
+});
 
 const title = "PJH's Portfolio";
 const description = "개발자 박준혁의 포트폴리오입니다.";
@@ -13,18 +16,18 @@ export const metadata: Metadata = {
   description: description,
   openGraph: {
     title: title,
-    description : description
-  }
-}
+    description: description,
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
