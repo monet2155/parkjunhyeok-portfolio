4d9e6d50c473a2c430f6ef83cbe006461ef098cb
Monett <monet9812@gmail.com>
2023-08-08 15:19:52 +0900

    feat: init Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+export default function Header() {
+  return (
+    <header
+      className={
+        "flex flex-row items-center justify-between fixed top-0 left-0 right-0 py-4 px-6"
+      }
+    >
+      <a className={"font-bold text-xl"}>Park Jun Hyeok&apos;s Portfolio</a>
+      <div className={"flex flex-row items-center gap-4"}>
+        <nav className={"max-md:hidden"}>
+          <ul className={"flex flex-row items-center gap-4"}>
+            <li>
+              <a>Summary</a>
+            </li>
+            <li>
+              <a>Career</a>
+            </li>
+            <li>
+              <a>Skill</a>
+            </li>
+            <li>
+              <a>Education</a>
+            </li>
+            <li>
+              <a>Etc</a>
+            </li>
+          </ul>
+        </nav>
+        <button className={"px-6 py-2 bg-[#5476FC] text-white rounded-2xl"}>
+          Contact
+        </button>
+      </div>
+    </header>
+  );
+}
