6ec8f40d7cf380d0bf7bde9ccefb10488d761594
Monett <monet9812@gmail.com>
2023-08-10 00:23:20 +0900

    feat: init fab

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Career from "@/components/Career";
 import Education from "@/components/Education";
 import Award from "@/components/Award";
 import Footer from "@/components/Footer";
+import FloatingButton from "@/components/FloatingButton";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Education />
       <Award />
       <Footer />
+      <FloatingButton />
     </main>
   );
 }
